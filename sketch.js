e1881a74936e18184091d87b109afe64facba951
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-04-14 22:13:33 -0700

    Kanye BETA
    Left and right motion with A and D keys.

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var QUAD_WIDTH, QUAD_WIDTH_HALF, QUAD_HEIGHT, NUM_ROWS;
+
+function setup() {
+    createCanvas(1400, 600);
+}
+
+function draw() {
+
+    for (var i = 0; i <= height; i++) {
+      for (var j = 0; j <= width; j++) {
+        fill(0, 13, 26);
+      }
+    }
+
+}
