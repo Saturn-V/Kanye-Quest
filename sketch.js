1ac33dafa19aecd384aefe7ee364ae82ebb83d38
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-04-15 19:10:09 -0700

    Kanye BETA .01
    Added correct files because I am a dummy.

--- sketch.js
+++ sketch.js
@@ -1,15 +1,33 @@
-var QUAD_WIDTH, QUAD_WIDTH_HALF, QUAD_HEIGHT, NUM_ROWS;
+var GRAVITY = .3;
+var GROUND, BG, PLAYER, ENEMY, FIRE;
+var GroundImg, BgImg, PlayerImg, EnemyImg, FireImg;
 
 function setup() {
-    createCanvas(1400, 600);
+  createCanvas(1400,600);
+
+  GROUND = createSprite(700, 550, 2800, 100);
+
+  PLAYER = createSprite(700, 500, 50, 50);
+
 }
 
 function draw() {
+  background(100, 150, 200);
 
-    for (var i = 0; i <= height; i++) {
-      for (var j = 0; j <= width; j++) {
-        fill(0, 13, 26);
-      }
-    }
 
+  if(keyDown("a")) {
+    PLAYER.position.x -= 4;
+  } else if(keyDown("d")) {
+    PLAYER.position.x += 4;
+  }
+
+  camera.position.x = PLAYER.position.x;
+  console.log(camera.position.x);
+  console.log("ground: " + GROUND.position.x);
+
+
+  PLAYER.collide(GROUND);
+
+
+  drawSprites();
 }
