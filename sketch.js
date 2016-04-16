fa58d21e0729c8108ea0b4b78e5413f141794cba
Alex Aaron Pena <alexaaronpena@gmail.com>
2016-04-15 23:10:15 -0700

    Landscape issues

--- sketch.js
+++ sketch.js
@@ -2,31 +2,43 @@ var GRAVITY = .3;
 var GROUND, BG, PLAYER, ENEMY, FIRE;
 var GroundImg, BgImg, PlayerImg, EnemyImg, FireImg;
 
+function preload() {
+
+    GroundImg = loadImage('http://i.imgur.com/vO9az9I.png');
+
+}
+
 function setup() {
   createCanvas(1400,600);
 
-  GROUND = createSprite(700, 550, 2800, 100);
+  GROUND = createSprite(width/2, 550);
+  GROUND.addImage(GroundImg);
 
-  PLAYER = createSprite(700, 500, 50, 50);
+  PLAYER = createSprite(700, 475, 50, 50);
 
 }
 
 function draw() {
-  background(100, 150, 200);
+  // background(100, 150, 200);
 
-
+  //Basic left and right movement for PLAYER aka KANYE aka YEEZUS
   if(keyDown("a")) {
     PLAYER.position.x -= 4;
   } else if(keyDown("d")) {
     PLAYER.position.x += 4;
   }
 
+  // Says that CAMERA will always follow PLAYER
   camera.position.x = PLAYER.position.x;
-  console.log(camera.position.x);
+
+  //My poor attempt to get the ground to wrap with movement
+  if(camera.position.x > GROUND.position.x - GROUND.width + width / 2) {
+    GROUND.position.x += GROUND.width;
+   }
+
+  //Check values here
+  console.log("camera: " + camera.position.x);
   console.log("ground: " + GROUND.position.x);
-
-
-  PLAYER.collide(GROUND);
 
 
   drawSprites();
